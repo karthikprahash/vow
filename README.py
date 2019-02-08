23aaede3e5adeb3bf3035e6dafdb86eebeef53c0
karthikprahash <46716288+karthikprahash@users.noreply.github.com>
2019-02-08 22:11:58 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,10 +1,16 @@
 #vow
 x=input('enter the letter')
 print(x)
+import string
+a=list(string.ascii_lowercase)
 s=('a','e','i','o','u')
 if x in s:
     print('vowel')
-else:
+elif x in a :
     print('consonent')
+else:
+    print('invalid input')
+
+
 
 
